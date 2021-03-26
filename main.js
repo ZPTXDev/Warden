893f046e3d23ff96f51cc07754c867780e3a240d
Max <max@zptx.icu>
2021-03-26 19:53:37 +0800

    fallback if no usage specified

--- main.js
+++ main.js
@@ -237,14 +237,25 @@ bot.on("messageCreate", msg => {
                     let result = actionFunction({prefix: prefix, cmd: cmd, body: body, guild: guild, message: msg});
                     console.log(`[C] ${guild ? `${msg.channel.guild.name} (${msg.channel.guild.id}) | ` : ""}${msg.author.username}#${msg.author.discriminator} (${msg.author.id}): ${msg.content}`);
                     if (!result) {
-                        let usage = modules[module][action]["usage"].replace(/%cmd%/g, cmd);
-                        msg.channel.createMessage({
-                            messageReferenceID: msg.id,
-                            embed: {
-                                description: `Usage: ${prefix}${usage}`,
-                                color: 0x2518a0
-                            }
-                        });
+                        if ("usage" in modules[module][action]) {
+                            let usage = modules[module][action]["usage"].replace(/%cmd%/g, cmd);
+                            msg.channel.createMessage({
+                                messageReferenceID: msg.id,
+                                embed: {
+                                    description: `Usage: ${prefix}${usage}`,
+                                    color: 0x2518a0
+                                }
+                            });
+                        }
+                        else {
+                            msg.channel.createMessage({
+                                messageReferenceID: msg.id,
+                                embed: {
+                                    description: `Command execution failed with no reason specified.`,
+                                    color: 0x2518a0
+                                }
+                            });
+                        }
                     }
                 }
             });
