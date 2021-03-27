eaaa2d83fcc4d978454af26e1748ede903b3c990
Max <max@zptx.icu>
2021-03-27 20:02:12 +0800

    separate the files for the sake of my sanity

--- modules/core/info.js
+++ modules/core/info.js
@@ -1,6 +1,7 @@
-const {SlashCommand, CommandOptionType} = require("slash-create");
-
-let action = function (details) {
+module.exports.commands = ["info"];
+module.exports.usage = "%cmd% [stats]";
+module.exports.description = "Display information about Warden.";
+module.exports.action = function (details) {
     const settings = require("../../main.js").settings;
     const managers = settings.get("managers");
     const build = require("../../main.js").build;
@@ -10,26 +11,15 @@ let action = function (details) {
     const msToTimeString = require("../../main.js").msToTimeString;
     const roundTo = require("../../main.js").roundTo;
     let type = "";
-    if ((details["slash"] && details["body"]["stats"]) || (!details["slash"] && details["body"] === "stats")) {
-        if (details["slash"] && !managers.includes(details["context"]["user"].id)) {
-            details["context"].send({
-                embeds: [
-                    {
-                        description: "You need to be a **Manager** to use that.",
-                        color: 0x2518a0
-                    }
-                ]
-            });
-            return;
-        }
-        else if (!details["slash"] && !managers.includes(details["message"].author.id)) {
+    if (details["body"] === "stats") {
+        if (!managers.includes(details["message"].author.id)) {
             return "manager";
         }
         else {
             type = "stats";
         }
     }
-    if (!details["slash"] && !["stats", ""].includes(details["body"])) {
+    if (!["stats", ""].includes(details["body"])) {
         return "usage";
     }
     let userTotal = 0;
@@ -43,70 +33,30 @@ let action = function (details) {
     Object.keys(modules).forEach(module => {
         actionsLoaded += Object.keys(modules[module]).length;
     });
-    let embed = {
-        title: "Warden",
-        description: `A moderation and utility bot, part of the ZapSquared Network.\nSource code available [here](https://github.com/zapteryx/Warden).\nRunning build [\`${build.slice(0, 7)}\`](https://github.com/zapteryx/Warden/commit/${build}).`,
-        color: 0x2518a0,
-        fields: type === "stats" ? [
-            {
-                name: "Bot Statistics",
-                value: `**Servers**: ${bot.guilds.size}\n**Users**: ${userTotal} (${bot.users.size} cached)\n**Channels**: ${channelTotal}`
-            },
-            {
-                name: "Technical Statistics",
-                value: `**RAM Usage**: ${roundTo(process.memoryUsage().heapUsed / 1024 / 1024, 2).toString()} MB\n**Uptime**: ${uptimeString}\n**Modules Loaded**: ${modulesLoaded} (${actionsLoaded} actions)`
-            },
-            {
-                name: "Version",
-                value: `**Warden**: \`${build}\`\n**Eris**: \`${require("eris").VERSION}\`\n**NodeJS**: \`${process.env.NODE_VERSION}\``
+    details["message"].channel.createMessage({
+        messageReferenceID: details["message"].id,
+        embed: {
+            title: "Warden",
+            description: `A moderation and utility bot, part of the ZapSquared Network.\nSource code available [here](https://github.com/zapteryx/Warden).\nRunning build [\`${build.slice(0, 7)}\`](https://github.com/zapteryx/Warden/commit/${build}).`,
+            color: 0x2518a0,
+            fields: type === "stats" ? [
+                {
+                    name: "Bot Statistics",
+                    value: `**Servers**: ${bot.guilds.size}\n**Users**: ${userTotal} (${bot.users.size} cached)\n**Channels**: ${channelTotal}`
+                },
+                {
+                    name: "Technical Statistics",
+                    value: `**RAM Usage**: ${roundTo(process.memoryUsage().heapUsed / 1024 / 1024, 2).toString()} MB\n**Uptime**: ${uptimeString}\n**Modules Loaded**: ${modulesLoaded} (${actionsLoaded} actions)`
+                },
+                {
+                    name: "Version",
+                    value: `**Warden**: \`${build}\`\n**Eris**: \`${require("eris").VERSION}\`\n**NodeJS**: \`${process.env.NODE_VERSION}\``
+                }
+            ] : [],
+            thumbnail: {
+                url: bot.user.avatarURL
             }
-        ] : [],
-        thumbnail: {
-            url: bot.user.avatarURL
         }
-    };
-    if (details["slash"]) {
-        details["context"].send({
-            embeds: [embed]
-        });
-    }
-    else {
-        details["message"].channel.createMessage({
-            messageReferenceID: details["message"].id,
-            embed: embed
-        });
-    }
+    });
     return true;
 };
-
-module.exports.slash = class InfoCommand extends SlashCommand {
-    constructor(creator) {
-        super(creator, {
-            name: "info",
-            description: "Display information about Warden.",
-            options: [
-                {
-                    name: "stats",
-                    description: "Display additional statistics about Warden.",
-                    required: false,
-                    type: CommandOptionType.BOOLEAN
-                }
-            ]
-        });
-    }
-    async run(ctx) {
-        let details = {
-            prefix: "/",
-            cmd: "info",
-            body: ctx.options,
-            guild: !!ctx.guildID,
-            context: ctx,
-            slash: true
-        };
-        action(details);
-    }
-}
-module.exports.commands = ["info"];
-module.exports.usage = "%cmd% [stats]";
-module.exports.description = "Display information about Warden.";
-module.exports.action = action;
