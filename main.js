2b62c74610e88640f3d653b081cc0270f6b0b213
Max <max@zptx.icu>
2021-03-30 02:28:39 +0800

    debug code

--- main.js
+++ main.js
@@ -278,25 +278,29 @@ function getUserId(cont, types=null, guildId) {
     }
     let userId;
     if (types.includes("mention") && cont.startsWith("<@") && cont.endsWith(">")) {
+        console.log("mention");
         userId = cont.replace(/<@!?/, "").replace(/>/, "");
         if (!bot.users.get(userId)) {
             userId = "";
         }
     }
     if (!userId && types.includes("id")) {
+        console.log("id");
         userId = cont;
         if (!bot.users.get(userId)) {
             userId = "";
         }
     }
     if (!userId && types.includes("nickname") && guildId) {
-        let guildMember = bot.guilds.get(guildId).members.find(u => u.nick && `${u.nick}` === `${cont}`);
+        console.log("nickname");
+        let guildMember = bot.guilds.get(guildId).members.find(u => u.nick && `${u.nick.toLowerCase()}` === `${cont.toLowerCase()}`);
         if (guildMember) {
             userId = guildMember.id;
         }
     }
     if (!userId && types.includes("username") && guildId) {
         if (cont.includes("#")) {
+            console.log("username 1");
             let split = cont.split("#");
             let guildMember = bot.guilds.get(guildId).members.find(u => `${u.username}#${u.discriminator}` === `${split[0]}#${split[1]}`);
             if (guildMember) {
@@ -304,7 +308,8 @@ function getUserId(cont, types=null, guildId) {
             }
         }
         else {
-            let guildMember = bot.guilds.get(guildId).members.find(u => `${u.username}` === `${cont}`);
+            console.log("username 2");
+            let guildMember = bot.guilds.get(guildId).members.find(u => `${u.username.toLowerCase()}` === `${cont.toLowerCase()}`);
             if (guildMember) {
                 userId = guildMember.id;
             }
