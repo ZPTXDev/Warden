570bb2f3786ea6c648fdfb72c70ec41addaf9033
Max <max@zptx.icu>
2021-03-30 09:48:12 +0800

    add avatar slash command

--- modules/utils/avatar.js
+++ modules/utils/avatar.js
@@ -39,3 +39,58 @@ module.exports.action = function (details) {
     });
     return true;
 }
+
+module.exports.slash = {
+    name: "avatar",
+    description: "Display a user's avatar.",
+    deferEphemeral: false,
+    options: [
+        {
+            name: "user",
+            description: "The user to display an avatar for.",
+            required: false,
+            type: CommandOptionType.USER
+        },
+        {
+            name: "options",
+            description: "Show all format and size variations for the avatar.",
+            required: false,
+            type: CommandOptionType.BOOLEAN
+        }
+    ]
+};
+module.exports.slashAction = async function(ctx) {
+    const bot = require("../../main.js").bot;
+    let userId;
+    let options = false;
+    if (ctx.options["options"]) {
+        options = true;
+    }
+    if (ctx.options["user"] && !!ctx.guildID) {
+        userId = ctx.options["user"];
+    }
+    else {
+        userId = ctx.user.id;
+    }
+    let user = bot.guilds.get(ctx.guildID).members.get(userId).user;
+    let animated = user.avatar.startsWith("a_");
+    let sizes = [];
+    let start = 4096;
+    while (start >= 16) {
+        sizes.push(`[${start.toString()}](${user.dynamicAvatarURL(animated ? "gif" : "png", start)})`);
+        start /= 2;
+    }
+    sizes = sizes.join(" | ");
+    await ctx.send({
+        embeds: [
+            {
+                title: `${user.username}#${user.discriminator}`,
+                description: `${options ? `**Formats**:\n${animated ? `[gif](${user.dynamicAvatarURL("gif", 2048)}) | ` : ""}[png](${user.dynamicAvatarURL("png", 2048)}) | [jpg](${user.dynamicAvatarURL("jpg", 2048)}) | [webp](${user.dynamicAvatarURL("webp", 2048)})\n**Sizes**:\n${sizes}` : `[External Link](${user.avatarURL})`}`,
+                image: {
+                    url: `${animated ? user.dynamicAvatarURL("gif", 2048) : user.dynamicAvatarURL("png", 2048)}`
+                },
+                color: 0x2518a0
+            }
+        ]
+    });
+}
