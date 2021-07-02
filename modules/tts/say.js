658836de9b0cb89491c308f2ccf4f87ae03152b0
Max <max@zptx.icu>
2021-07-02 09:25:49 +0800

    initial tts test

---
+++ modules/tts/say.js
@@ -0,0 +1,114 @@
+const {getPermsMatch, settings, bot} = require("../../main");
+const {CommandOptionType} = require("slash-create");
+
+module.exports.commands = ["say"];
+module.exports.usage = "%cmd% message"
+module.exports.description = "Use TTS to say something in your voice channel.";
+module.exports.action = async function (details) {
+    if (details["body"] === "") {
+        return "usage";
+    }
+    let botPermsMissing = getPermsMatch(details["message"].channel.guild.members.get(bot.user.id).permissions, ["voiceConnect", "voiceSpeak"]);
+    if (botPermsMissing.length > 0) {
+        return ["self"].concat(botPermsMissing);
+    }
+    let result = await common(details["guild"].id, details["message"].author.id, details["message"].channel.id, details["body"]);
+    let resultString = "";
+    switch (result) {
+        case "disabled":
+            resultString = "This feature was not set up and is disabled.";
+            break;
+        case "vc":
+            resultString = "You are not in a voice channel.";
+            break;
+        case "different":
+            resultString = "You are not in the same voice channel as me.";
+            break;
+        case "busy":
+            resultString = "I'm currently in the middle of another TTS message.";
+            break;
+        case "success":
+            resultString = `"**${details["message"].author.username}**: ${details["body"]}`;
+            break;
+    }
+    if (resultString) {
+        await details["message"].channel.createMessage({
+            messageReference: {messageID: details["message"].id},
+            embed: {
+                description: resultString,
+                color: 0x2518a0
+            }
+        });
+    }
+
+}
+
+module.exports.slash = {
+    name: "say",
+    description: "Use TTS to say something in your voice channel.",
+    deferEphemeral: false,
+    options: [
+        {
+            name: "message",
+            description: "The message to say.",
+            required: true,
+            type: CommandOptionType.STRING
+        }
+    ],
+    guildOnly: true
+}
+module.exports.slashAction = async function (ctx) {
+    let botPermsMissing = getPermsMatch(bot.guilds.get(ctx.guildID).members.get(bot.user.id).permissions, ["voiceConnect", "voiceSpeak"]);
+    if (botPermsMissing.length > 0) {
+        await require("../../main.js").slashPermissionRejection(ctx, ["self"].concat(botPermsMissing));
+        return;
+    }
+    let result = await common(ctx.guildID, ctx.user.id, ctx.channelID, ctx.options["message"]);
+    let resultString = "";
+    switch (result) {
+        case "disabled":
+            resultString = "This feature was not set up and is disabled.";
+            break;
+        case "vc":
+            resultString = "You are not in a voice channel.";
+            break;
+        case "different":
+            resultString = "You are not in the same voice channel as me.";
+            break;
+        case "busy":
+            resultString = "I'm currently in the middle of another TTS message.";
+            break;
+        case "success":
+            resultString = `"**${ctx.user.username}**: ${ctx.options["message"]}`;
+            break;
+    }
+    if (resultString) {
+        await ctx.send({
+            embeds: [
+                {
+                    description: resultString,
+                    color: 0x2518a0
+                }
+            ],
+            ephemeral: true
+        });
+    }
+}
+
+async function common(gid, uid, cid, text) {
+    if (!settings.get("llnodes")) {
+        return "disabled";
+    }
+    if (!bot.guilds.get(gid).members.get(uid).voiceState.channelID) {
+        return "vc";
+    }
+    if (bot.guilds.get(gid).members.get(bot.user.id).voiceState.channelID && bot.guilds.get(gid).members.get(bot.user.id).voiceState.channelID !== bot.guilds.get(gid).members.get(uid).voiceState.channelID) {
+        return "different";
+    }
+    let {ttsQueue, tts} = require("./ttshandler");
+    if (gid in ttsQueue) {
+        return "busy";
+    }
+    await tts(bot.guilds.get(gid).channels.get(cid), text);
+    return "success";
+}
---
+++ modules/tts/ttshandler.js
@@ -0,0 +1,78 @@
+const { PlayerManager } = require("eris-lavalink");
+const superagent = require("superagent");
+const googleTTS = require('google-tts-api');
+
+let ttsQueue = {};
+let timeouts = {};
+const {bot, settings} = require("../../main");
+
+function getPlayer(channel) {
+    if (!channel || !channel.guild) {
+        return Promise.reject('Not a guild channel.');
+    }
+    let player = bot.voiceConnections.get(channel.guild.id);
+    if (player) {
+        return {p: Promise.resolve(player), n: false};
+    }
+    return {p: bot.joinVoiceChannel(channel.id), n: true};
+}
+
+async function resolveTracks(node, search) {
+    const result = await superagent.get(`http://${node.host}:${node.port}/loadtracks?identifier=${search}`)
+        .set('Authorization', node.password)
+        .set('Accept', 'application/json');
+    if (!result) {
+        throw "Unable to play that track.";
+    }
+    return result.body;
+}
+
+async function tts(channel, text) {
+    if (!(bot.voiceConnections instanceof PlayerManager)) {
+        bot.voiceConnections = new PlayerManager(bot, nodes, {
+            numShards: bot.shards.size, // number of shards
+            userId: bot.user.id // the user id of the bot
+        });
+    }
+    let player = await getPlayer(channel);
+    let nPlayer = player.n;
+    player = player.p;
+    if (nPlayer) {
+        function left() {
+            delete ttsQueue[channel.guild.id];
+        }
+        player.on("disconnect", left);
+        player.on("error", left);
+        player.on("stuck", left);
+        player.on("end", d => {
+            if (d.reason && d.reason === 'REPLACED') {return;}
+            if (ttsQueue[channel.guild.id].length > 0) {
+                player.play(ttsQueue[channel.guild.id][0]);
+                ttsQueue[channel.guild.id].shift();
+            }
+            else {
+                delete ttsQueue[channel.guild.id];
+                timeouts[channel.guild.id] = setTimeout(cid => {
+                    bot.leaveVoiceChannel(cid);
+                }, 300000, channel.id);
+            }
+        });
+    }
+    if (channel.guild.id in timeouts) {
+        clearTimeout(timeouts[channel.guild.id]);
+        delete timeouts[channel.guild.id];
+    }
+    const urls = googleTTS.getAllAudioUrls(text, {
+        splitPunct: ',.?'
+    });
+    ttsQueue[channel.guild.id] = [];
+    for (const u of urls) {
+        let track = await resolveTracks(settings.get("llnodes"), u.url);
+        ttsQueue[channel.guild.id].push(track);
+    }
+    player.play(ttsQueue[channel.guild.id][0]);
+    ttsQueue[channel.guild.id].shift();
+}
+
+module.exports.ttsQueue = ttsQueue;
+module.exports.tts = tts;
