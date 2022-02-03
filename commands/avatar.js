856f7f6a482b27a815691eceaeeef062545915f9
Xayanide <chris.balili.91@gmail.com>
2022-02-03 13:48:52 +0800

    refactor(locales): use CMD_AVATAR_USER instead

--- commands/avatar.js
+++ commands/avatar.js
@@ -65,7 +65,7 @@ module.exports = {
 		await interaction.reply({
 			embeds: [
 				new MessageEmbed()
-					.setTitle(getLocale(defaultLocale, 'CMD_AVATAR_USER_AVATAR'))
+					.setTitle(getLocale(defaultLocale, 'CMD_AVATAR_USER'))
 					.setDescription(options ? `${getLocale(defaultLocale, 'CMD_AVATAR_FORMATS')}\n${animated ? `[gif](${user.avatarURL({ dynamic: true, size: 2048 })}) | ` : ''}[png](${user.avatarURL({ format: 'png', size: 2048 })}) | [jpg](${user.avatarURL({ format: 'jpg', size: 2048 })}) | [webp](${user.avatarURL({ format: 'webp', size: 2048 })})\n${getLocale(defaultLocale, 'CMD_AVATAR_SIZES')}\n${sizes}` : `[${getLocale(defaultLocale, 'CMD_AVATAR_EXTERNAL')}](${user.avatarURL({ dynamic: true, format: 'png' })})`)
 					.setAuthor(user.tag)
 					.setImage(user.displayAvatarURL({ dynamic: true, format: 'png', size: 2048 }))
