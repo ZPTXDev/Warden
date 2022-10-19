ab04bbcb37fb74c01ef881ed36f1960712431190
Max <max@zptx.dev>
2022-10-19 10:46:21 +0800

    fix: crash relating to leaving guild while session is active

--- src/events/guildDelete.ts
+++ src/events/guildDelete.ts
@@ -1,13 +1,23 @@
+import type { WardenClient, WardenPlayer } from '#src/lib/util/common.d.js';
 import { logger } from '#src/lib/util/common.js';
 import type { Guild } from 'discord.js';
 
 export default {
     name: 'guildDelete',
     once: false,
-    execute(guild: Guild): void {
+    async execute(guild: Guild & { client: WardenClient }): Promise<void> {
         logger.info({
             message: `[G ${guild.id}] Left guild ${guild.name}`,
             label: 'Discord',
         });
+        const player = guild.client.tts.players.get(guild.id) as WardenPlayer;
+        if (player) {
+            logger.info({
+                message: `[G ${guild.id}] Cleaning up`,
+                label: 'Warden',
+            });
+            player.channelId = null;
+            return player.handler.disconnect();
+        }
     },
 };
